de298a4d9f2994d6c18e434c7ac9dc31f03ecb52
Mikołaj Kąkol <mkakol@speednet.pl>
2022-12-19 14:38:22 +0100

    Benchmark (#1)
    * added benchmarks

--- app/src/main/java/com/mikolajkakol/myapplication/MainActivity.kt
+++ app/src/main/java/com/mikolajkakol/myapplication/MainActivity.kt
@@ -3,13 +3,22 @@ package com.mikolajkakol.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.mikolajkakol.fontui.ShadersComposition
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.mikolajkakol.fontui.*
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -19,9 +28,41 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    ShadersComposition()
+
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = "list") {
+                        composable("list") { Composables(navController) }
+                        composable("shaderPerf1") {
+                            repeat(30) {
+                                ShaderPerformance1()
+                            }
+                        }
+                        composable("shaderPerf2") {
+                            repeat(30) {
+                                ShaderPerformance2()
+                            }
+                        }
+                        composable("shaderPerf3") {
+                            repeat(30) {
+                                ShaderPerformance3()
+                            }
+                        }
+                    }
                 }
             }
+        }
+    }
+
+    @Composable
+    private fun Composables(navController: NavHostController) = Column {
+        Button(onClick = { navController.navigate("shaderPerf1") }) {
+            Text(text = "Shader performance 1")
+        }
+        Button(onClick = { navController.navigate("shaderPerf2") }) {
+            Text(text = "Shader performance 2")
+        }
+        Button(onClick = { navController.navigate("shaderPerf3") }) {
+            Text(text = "Shader performance 3")
         }
     }
 }
